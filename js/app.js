6e57db3f318b47e7a8d2dfa6ff43c726e7bf128e
GennaSimpson <97615205+GennaSimpson@users.noreply.github.com>
2022-02-02 13:40:06 -0500

    Added word arrays

--- js/app.js
+++ js/app.js
@@ -8,6 +8,12 @@ var synth = window.speechSynthesis;
 var textToSpeak = 'This is the text string that you will generate with your script';
 var speakButton = document.querySelector('button');
 
+// Five Arrays for Story Button Words
+let subject = ['the turkey', 'Mom', 'Dad', 'The Dog', 'My teacher', 'The elephant'];
+let verb = ['sat on', 'ate', 'danced with', 'saw', 'doesn\'t like', 'kissed' ];
+let adjective = ['a funny', 'a scary', 'a goofy', 'a slimy', 'a barking', 'a fat'];
+let thing = ['goat', 'monkey', 'fish', 'cow', 'frog', 'bug', 'worm'];
+let place = ['on the moon', 'on the chair', 'in my spaghetti', 'in my soup', 'on the grass', 'in my shoes'];
 
 /* Functions
 -------------------------------------------------- */
